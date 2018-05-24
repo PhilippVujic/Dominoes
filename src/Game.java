d433c94f996d9abbdaba36cb6f5308e726237e7c
Phil <newtimestube@gmail.com>
2018-05-24 13:03:43 +0200

    createPieces.java

--- src/Game.java
+++ src/Game.java
@@ -1,14 +1,21 @@
+import Piece.PieceContainer;
 import Player.Player;
 
 public class Game {
 
+    private int pieceSet = 6;
     Player[] player = new Player[2];
+    PieceContainer pieceContainer;
 
     public Game() {
 
     }
 
     public void start() {
+        pieceContainer = new PieceContainer(pieceSet + 1 * (pieceSet + 2) / 2, pieceSet);
+    }
 
+    public int getPieceSet() {
+        return pieceSet;
     }
 }
--- src/Piece/PieceContainer.java
+++ src/Piece/PieceContainer.java
@@ -3,8 +3,20 @@ package Piece;
 public class PieceContainer {
 
     int quantityPieces;
+    Piece[] pieces;
+    int pieceSet;
 
-    public PieceContainer(int quantityPieces) {
+    public PieceContainer(int quantityPieces, int pieceSet) {
         this.quantityPieces = quantityPieces;
+        pieces = new Piece[quantityPieces];
+        this.pieceSet = pieceSet;
+
+        createPieces(pieceSet);
+    }
+
+    private void createPieces(int pieceSet) {
+        for (int i = 0; i < pieceSet + 1; i++) {
+
+        }
     }
 }
